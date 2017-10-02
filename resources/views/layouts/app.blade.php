8e53b07988accd8b31537d5c2578c177fdd96550
Quang Huy <dqhuy78@gmail.com>
2017-10-02 13:25:08 +0700

    Init static page
    Remove ignore image
    
    Fix some bug

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" href="images/ico/favicon.png">
+
+    {{ HTML::style('bowerrc/bootstrap/dist/css/bootstrap.min.css') }}
+    {{ HTML::style('css/main.css') }}
+    {{ HTML::style('css/plugin.css') }}
+    {{ HTML::style('css/style.css') }}
+
+    @yield('style')
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('pageTitle')</title>
+</head>
+<body class="home transparent-header">
+    <div id="introLoader" class="introLoading"></div>
+
+    <div class="container-wrapper">
+        @include('layouts.header')
+
+        <div class="main-wrapper scrollspy-container">
+            <div class="hero img-bg-01">
+				<div class="container">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+
+        @include('layouts.footer')
+    </div>
+
+    @include('layouts.login')
+    @include('layouts.signup')
+    @include('layouts.forgot_password')
+
+
+    {{ HTML::script('bowerrc/jquery/dist/jquery.min.js') }}
+    {{ HTML::script('bowerrc/jquery-migrate-3.0.1/index.js') }}
+    {{ HTML::script('js/core-plugins.js') }}
+    {{ HTML::script('js/customs.js') }}
+    {{ HTML::script('js/loading.js') }}
+
+    @yield('script')
+</body>
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,160 @@
+<header id="header">
+    <nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function navbar-arrow">
+        <div class="container">
+            <div class="logo-wrapper">
+                <div class="logo">
+                    <a href="index-2.html"><img src="images/logo-white.png" alt="Logo" /></a>
+                </div>
+            </div>
+
+            <div id="navbar" class="navbar-nav-wrapper">
+                <ul class="nav navbar-nav" id="responsive-menu">
+                    <li>
+                        <a href="#">{{__('Home')}}</a>
+                    </li>
+
+                    <li>
+                        <a href="#">{{__('Product')}}</a>
+                        <ul>
+                            <li>
+                                <a href="#">{{__('Fishes')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('FishsFood')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('FishTanks')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('SandGravel')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('3DLanscape')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('Plants')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('Filters')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('AquariumLight')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('NewArrival')}}</a></li>
+                                    <li><a href="#">{{__('TrendingItems')}}</a></li>
+                                    <li><a href="#">{{__('AllItems')}}</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">{{__('Blueprints')}}</a>
+                        <ul>
+                            <li>
+                                <a href="#">{{__('NewBlueprints')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('TrendingBlueprints')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('AllBlueprint')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">{{__('Posts')}}</a>
+                        <ul>
+                            <li>
+                                <a href="#">{{__('NewPosts')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('TrendingPosts')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">{{__('AllPost')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">{{__('MyBlueprints')}}</a>
+                        <ul>
+                            <li>
+                                <a href="#">{{__('AllBlueprints')}}</a>
+                                <ul>
+                                    <li><a href="#">{{__('FishTanks')}}</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">{{__('CloneBlueprints')}}</a></li>
+                            <li><a href="#">{{__('CreateBlueprints')}}</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-mini-wrapper">
+                <ul class="nav-mini">
+                    <li><a data-toggle="modal" href="#registerModal"><i class="icon-user-follow" data-toggle="tooltip" data-placement="bottom" title="sign up"></i></a></li>
+                    <li><a data-toggle="modal" href="#loginModal"><i class="icon-login" data-toggle="tooltip" data-placement="bottom" title="login"></i> </a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="slicknav-mobile"></div>
+    </nav>
+</header>
