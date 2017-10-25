283ac511ffafa3964fa5615833c299e287ac5990
dangminhtruong <xdangminhtruongx@gmail.com>
2017-10-25 22:09:42 +0700

    first

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\Contracts\PostRepositoryInterface as PostRepository;
 
 class PostController extends Controller
 {
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     public function writePost()
     {
         return view('post.write_post');
+    }
+
+    public function changePushlish($id)
+    {
+        return $this->postRepository->changePublishStatus($id);
     }
 }
